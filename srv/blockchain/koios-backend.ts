3073e3732af0d9e706f3b6064b5e9e2d423e94cc
maxalexweber1 <max@maxalexweber.de>
2025-12-05 08:21:26 +0100

    feat: implement getLatestBlock and getLatestEpoch methods for koios backend

--- srv/blockchain/koios-backend.ts
+++ srv/blockchain/koios-backend.ts
@@ -53,12 +53,61 @@ export class KoiosBackend implements CardanoBackend {
    };
   }
 
-  getLatestBlock(): Promise<LatestBlock> {
-    throw new Error('NOT_SUPPORTED');
+  async getLatestBlock(): Promise<LatestBlock> {
+    try {
+      // get tip first
+      const tipData = await this.api.get('/tip');
+
+      const blockHash = tipData.data.hash;
+      // get data of the tip block
+      const blockData = await this.api.post('/block_info', { _block_hashes: [blockHash] });
+
+      if (blockData.data && blockData.data.length > 0) {
+
+        return {
+          time: blockData.data[0].time,
+          height: blockData.data[0].block_height,
+          hash: blockData.data[0].block_hash,
+          slot: blockData.data[0].slot_no,
+          epoch: blockData.data[0].epoch_no,
+          epochSlot: blockData.data[0].epoch_slot_no,
+          slotLeader: blockData.data[0].vrf_key,
+          size: blockData.data[0].block_size,
+          txCount: blockData.data[0].tx_count,
+          fees: blockData.data[0].total_fees,
+   };
+  }
+    throw new Error('ERROR_FETCHING_BLOCK');
+  } catch (err: any) {
+      if (err?.status === 404 || err?.response?.status === 404) {
+        throw new Error('NOT_FOUND');
+      }
+      throw err;
+    }
   }
 
-  getLatestEpoch(): Promise<LatestEpoch> {
-    throw new Error('NOT_SUPPORTED');
+  async getLatestEpoch(): Promise<LatestEpoch> {
+     // get tip first
+      const tipData = await this.api.get('/tip');
+
+      // get data of the tip epoch
+      const epochData = await this.api.post('/epoch_info', { _epoch_nos: [tipData.data.epoch_no] });
+      if (epochData.data && epochData.data.length > 0) {
+
+        return {  
+          epoch: epochData.data[0].epoch_no,
+          start_time: epochData.data[0].start_time,
+          end_time: epochData.data[0].end_time,
+          first_block_time: epochData.data[0].first_block_time,
+          last_block_time: epochData.data[0].last_block_time,
+          block_count: epochData.data[0].block_count,
+          tx_count: epochData.data[0].tx_count,
+          output: epochData.data[0].total_output,
+          fees: epochData.data[0].total_fees,
+          active_stake: epochData.data[0].active_stake,
+     };
+    }
+    throw new Error('ERROR_FETCHING_EPOCH');
   }
 
   async getAddress(address: string): Promise<any> {
